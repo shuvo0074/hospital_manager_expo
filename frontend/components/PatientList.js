836864842e1548d59eeed4b4b847007f320a5d83
Tarmim Hoque Shuvo <tarmimhoqueshuvo@gmail.com>
2025-05-11 06:33:39 +0600

    add error handling

--- frontend/components/PatientList.js
+++ frontend/components/PatientList.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, TextInput, FlatList, Text, Button, ActivityIndicator } from 'react-native';
+import { View, TextInput, FlatList, Text, Button, ActivityIndicator, Dimensions, Alert } from 'react-native';
 import { fetchPatients } from '../services/api';
 import useDebounce from '../hooks/useDebounce';
 
@@ -10,25 +10,60 @@ export default function PatientList({ navigation }) {
   const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(false);
   const [next, setNext] = useState(true);
+  const { height } = Dimensions.get('screen')
+
+  // show text to user if there is no patient visible on screen
+  const ListEmptyComponent = _ => (
+    <View
+      style={{
+        backgroundColor: '#e3e3e3',
+        height: height - 150,
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}
+    >
+      <Text
+        style={{
+          textAlign: 'center'
+        }}
+      >
+        Please search for a patient name above
+      </Text>
+    </View>
+  )
+
 
   useEffect(() => {
-    setLoading(true);
-    fetchPatients(debouncedQ, 1).then(res => {
-      setData(res.data);
-      setNext(!!res.nextPage);
-      setPage(1);
-      setLoading(false);
-    });
+    if (q.length) { // if search query is not empty
+      setLoading(true);
+      fetchPatients(debouncedQ, 1).then(res => {
+        setData(res.data);
+        setNext(!!res.nextPage);
+        setPage(1);
+        setLoading(false);
+        if (res.error)
+          Alert.alert("Error!", res.error)
+      });
+    }
   }, [debouncedQ]);
 
+  useEffect(_ => {
+    if (!q.length)
+      setData([])
+  }, [q])
+
   const loadMore = () => {
-    if (!next || loading) return;
+    // end reached threshold for flatlist is set to 0.2, which means if last two items are visible, the flatlist will load more data
+    // Load more is being called twice for the first time because all 10 items are displayed because of large screen height and it is reaching the threshold
+    if (!next || loading || !q.length) return;
     setLoading(true);
     fetchPatients(debouncedQ, page + 1).then(res => {
       setData(d => [...d, ...res.data]);
       setNext(!!res.nextPage);
       setPage(p => p + 1);
       setLoading(false);
+      if (res.error)
+        Alert.alert("Error!", res.error)
     });
   };
 
@@ -47,8 +82,9 @@ export default function PatientList({ navigation }) {
           data={data}
           keyExtractor={i => i.id}
           onEndReached={loadMore}
-          onEndReachedThreshold={0.5}
+          onEndReachedThreshold={0.2}
           ListFooterComponent={loading ? <ActivityIndicator /> : null}
+          ListEmptyComponent={<ListEmptyComponent />}
           renderItem={({ item }) => (
             <View style={{ marginBottom: 8 }}>
               <Text>{item.name}</Text>
